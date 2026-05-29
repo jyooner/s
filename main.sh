4d496645b17895ab71229a70ee0c0ecced5d3927
Jeongseop Yoon <jeongseop_yoon@myabcusd.org>
2026-05-29 16:09:32 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,4 @@
 sudo useradd -m -s /bin/bash robocamp && echo "robocamp:robocamp" | sudo chpasswd && /
 su - robocamp
+sudo apt install chromium-browser -y
+google-chrome --enable-experimental-web-platform-featuers
