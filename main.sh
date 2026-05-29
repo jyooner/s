dfddd10581b63a76d5f312191bf2b18781a0c06f
Jeongseop Yoon <jeongseop_yoon@myabcusd.org>
2026-05-29 16:13:31 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,18 @@
-sudo useradd -m -s /bin/bash robocamp && echo "robocamp:robocamp" | sudo chpasswd && /
-su - robocamp
-sudo apt install chromium-browser -y
-google-chrome --enable-experimental-web-platform-features
+#!/bin/bash
+
+# 1. Create the user
+sudo useradd -m -s /bin/bash robocamp
+
+# 2. Set the password
+echo "robocamp:robocamp" | sudo chpasswd
+
+# 3. Add robocamp to the sudo group so it actually has permission to install things
+sudo usermod -aG sudo robocamp
+
+# 4. Run the installations AS the robocamp user
+sudo -u robocamp sudo apt update
+sudo -u robocamp sudo apt install chromium-browser -y
+
+# Note: Google Chrome is usually not in the default apt repositories for Pop!_OS. 
+# If you meant to launch Chrome with that flag *after* installing Chromium, use this:
+sudo -u robocamp chromium-browser --enable-experimental-web-platform-features &
